523f1bcb7a47f575797ca321782fcc92e471d0ab
Bolivar Stephen <bolivar.stephen@gmail.com>
2021-06-15 12:47:57 -0400

    fix: test

--- test/config.ts
+++ test/config.ts
@@ -7,10 +7,10 @@ if (process.env.LOAD_ENV) {
     path: path.join(__dirname, '../.env'),
   });
 }
-const DEFAULT_CLIENT_ID = 'dummy-acme-client-id';
-const DEFAULT_CLIENT_SECRET = 'dummy-acme-client-secret';
+const DEFAULT_API_KEY = 'dummy-api-key';
+const DEFAULT_API_URL = 'https://api.hubapi.com';
 
 export const integrationConfig: IntegrationConfig = {
-  clientId: process.env.CLIENT_ID || DEFAULT_CLIENT_ID,
-  clientSecret: process.env.CLIENT_SECRET || DEFAULT_CLIENT_SECRET,
+  apiKey: process.env.API_KEY || DEFAULT_API_KEY,
+  apiBaseUrl: process.env.CLIENT_ID || DEFAULT_API_URL,
 };
