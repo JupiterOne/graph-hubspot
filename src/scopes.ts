0f54c47e85e27a0d5d3f99f838e6e5275ef8c714
Adam Pierson <adam.pierson@jupiterone.com>
2022-08-10 07:18:39 -0500

    Updating scopes to stop using the no longer available contacts scope.

--- src/scopes.ts
+++ src/scopes.ts
@@ -1,4 +1,10 @@
-export const COMPANY_SCOPES = ['contacts'];
-export const OWNER_SCOPES = ['contacts', 'crm.objects.owners.read'];
+export const COMPANY_SCOPES = [
+  'crm.objects.contacts.read',
+  'crm.objects.companies.read',
+];
+export const OWNER_SCOPES = [
+  'crm.objects.contacts.read',
+  'crm.objects.owners.read',
+];
 export const ROLE_SCOPES = ['settings.users.read'];
 export const USER_SCOPES = ['settings.users.read'];
